6017f1de3d25ac66d31c6b298d3e6dda20120ff4
DeltaRobotics-FTC <deltaroboticsftc@gmail.com>
2025-07-01 20:58:23 -0500

    nothing

--- TeamCode/src/main/java/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -7,17 +7,18 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class LConstants {
     static {
-        PinpointConstants.forwardY = 6.35;
-        PinpointConstants.strafeX = -171.45;
-        PinpointConstants.distanceUnit = DistanceUnit.MM;
-        PinpointConstants.hardwareMapName = "odo";
-        PinpointConstants.useYawScalar = false;
-        PinpointConstants.yawScalar = 1.0;
-        PinpointConstants.useCustomEncoderResolution = false;
-        PinpointConstants.encoderResolution = GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD;
-        PinpointConstants.customEncoderResolution = 13.26291192;
-        PinpointConstants.forwardEncoderDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
-        PinpointConstants.strafeEncoderDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
+        ThreeWheelConstants.forwardTicksToInches = .001989436789;
+        ThreeWheelConstants.strafeTicksToInches = .001989436789;
+        ThreeWheelConstants.turnTicksToInches = .001989436789;
+        ThreeWheelConstants.leftY = 1;
+        ThreeWheelConstants.rightY = -1;
+        ThreeWheelConstants.strafeX = -2.5;
+        ThreeWheelConstants.leftEncoder_HardwareMapName = "motorLB";
+        ThreeWheelConstants.rightEncoder_HardwareMapName = "motorLF";
+        ThreeWheelConstants.strafeEncoder_HardwareMapName = "motorRF";
+        ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE;
+        ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE;
+        ThreeWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
     }
 }
 
